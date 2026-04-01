db37f2251df9e5a3ebc334e947d5a367a649546f
akhilag05 <akhileshgowdaksag05@gmail.com>
2026-04-01 15:57:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function setTheme(theme) {
+    document.body.className = theme;
+}
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+reveals.forEach(section => {
+    observer.observe(section);
+});
